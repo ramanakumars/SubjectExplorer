0e7d4e22bef85bdbe921630b04d185ab600c8981
Ramanakumar Sankar <rsankar@umn.edu>
2023-05-06 10:44:52 -0500

    moved fetch from python to js

--- frontend/src/util/zoo_utils.js
+++ frontend/src/util/zoo_utils.js
@@ -1,20 +1,18 @@
 const BATCH_SIZE = 100;
 
 const getImageUrl = (media) => {
-	if (media.length === 1) {
-		if ('image/png' in media[0]) {
-			return media[0]['image/png'];
-		} else if ('image/jpeg' in media[0]) {
-			return media[0]['image/jpeg'];
+	return media.map((url) => {
+		if ('image/png' in url) {
+			return url['image/png'];
+		} else if ('image/jpeg' in url) {
+			return url['image/jpeg'];
 		} else {
-			console.log(media);
+			console.log(url);
 		}
-	} else {
-		console.log(media);
-	}
+	});
 }
 
-const getJSONData = async (url) => {
+export const getJSONData = async (url) => {
 	return fetch(url, {
 		method: "GET",
 		headers: {
@@ -24,27 +22,37 @@ const getJSONData = async (url) => {
 	}).then((result) => {
 		return result.json();
 	});
+}
 
+export const getProjects = async () => {
+	const project_ids = [17032, 16696, 14993]
+
+	return await Promise.all(project_ids.map(async (project_id) => {
+		return getJSONData("https://www.zooniverse.org/api/projects/" + project_id)
+			.then((data) => {
+				return {id: data.projects[0].id, name: data.projects[0].display_name};
+			});
+	}));
 }
 
 export const getAvatarSrc = async (project_id) => {
 	return getJSONData("https://www.zooniverse.org/api/projects/" + project_id + "/avatar/")
-	  .then((data) => {
-		  return data.media[0].src;
-	  });
+		.then((data) => {
+			return data.media[0].src;
+		});
 }
 
 export const getWorkflowData = async (workflow_id) => {
 	return getJSONData("https://www.zooniverse.org/api/workflows/" + workflow_id)
-	  .then((data) => {
-		  let workflow = data.workflows[0];
-		  return {
-			  id: workflow_id,
-			  name: workflow.display_name,
-			  subject_sets: workflow.links.subject_sets, 
-			  subject_count: workflow.subjects_count
-		  }
-	  });
+		.then((data) => {
+			let workflow = data.workflows[0];
+			return {
+				id: workflow_id,
+				name: workflow.display_name,
+				subject_sets: workflow.links.subject_sets,
+				subject_count: workflow.subjects_count
+			}
+		});
 }
 
 export const getSubjects = async (workflow_id) => {
@@ -65,16 +73,16 @@ export const getSubjectsOld = async (subject_sets) => {
 }
 
 export const getSubjectFromSubjectSet = async (subject_set) => {
-	return 	getJSONData("https://www.zooniverse.org/api/subject_sets/" + subject_set + "/")
+	return getJSONData("https://www.zooniverse.org/api/subject_sets/" + subject_set + "/")
 		.then(async (data) => {
 			let nsubjects = data.subject_sets[0].set_member_subjects_count;
-			let npages = parseInt( nsubjects / BATCH_SIZE ) + 1;
+			let npages = parseInt(nsubjects / BATCH_SIZE) + 1;
 
 			let promises = []
-			for(let i=1; i<=npages; i++) {
+			for (let i = 1; i <= npages; i++) {
 				promises.push(getJSONData("https://www.zooniverse.org/api/subjects?subject_set_id=" + subject_set + "&page_size=" + BATCH_SIZE + "&page=" + i)
 					.then((data) => {
-						return 	data.subjects;
+						return data.subjects;
 					})
 				);
 			}
@@ -87,6 +95,6 @@ export const getSubjectFromSubjectSet = async (subject_set) => {
 						urls: getImageUrl(subject.locations)
 					};
 				});
-			})		
+			})
 		});
 }
