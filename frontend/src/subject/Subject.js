7881ef0bca4e343ef642b8fc4adc5abede41d2fe
Ramanakumar Sankar <rsankar@umn.edu>
2023-07-11 15:50:44 -0500

    modifying subject to functional component

--- frontend/src/subject/Subject.js
+++ frontend/src/subject/Subject.js
@@ -1,49 +1,28 @@
 import React from "react";
 
-export default class Subject extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            ...props.metadata
-        };
-    }
-
-    render() {
-        return (
-            <span key={this.state.subject_ID + "_span"} style={this.state.style}>
-                <a href={"/subject/" + this.state.subject_ID} target="_blank" rel="noreferrer">
-                    {this.state.url.length === 1 &&
-                    <SingleSubjectImage
-                        subject_ID={this.state.subject_ID}
-                        url={this.state.url}
-                    />
-                    }
-                </a>
-            </span>
-        );
-    }
+export default function Subject({metadata}) {
+    return (
+        <span key={metadata.subject_ID + "_span"} style={metadata.style}>
+            <a href={"/subject/" + metadata.subject_ID} target="_blank" rel="noreferrer">
+                {metadata.url.length === 1 &&
+                <SingleSubjectImage
+                    subject_ID={metadata.subject_ID}
+                    url={metadata.url}
+                />
+                }
+            </a>
+        </span>
+    );
 }
 
-class SingleSubjectImage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            url: props.url,
-            subject_ID: props.subject_ID
-        };
-    }
-
-    render() {
-        return (
-            <img
-                key={this.state.subject_ID + "_img"}
-                src={this.state.url}
-                alt={this.state.subject_ID}
-                title={this.state.subject_ID}
-                className="subject-image"
-            />
-        )
-    }
+function SingleSubjectImage({ url, subject_ID }) {
+    return (
+        <img
+            key={subject_ID + "_img"}
+            src={url}
+            alt={subject_ID}
+            title={subject_ID}
+            className="subject-image"
+        />
+    )
 }
